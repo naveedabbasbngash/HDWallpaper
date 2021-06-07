817540bd22ad162d96c82be04cafe08c5cfaf473
naveedabbas <naveedabbasbngsh@gmail.com>
2021-06-07 13:18:45 +0500

    ZS wALLPAPER

---
+++ app/src/main/java/zs/wallpapers/site/SampleModel.java
@@ -0,0 +1,17 @@
+package zs.wallpapers.site;
+
+public class SampleModel {
+    String picPath;
+
+    public SampleModel(String picPath) {
+        this.picPath = picPath;
+    }
+
+    public String getPicPath() {
+        return picPath;
+    }
+
+    public void setPicPath(String picPath) {
+        this.picPath = picPath;
+    }
+}
